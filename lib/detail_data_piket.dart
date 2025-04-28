dc37a019843f73dbc92af5cd51f20a06d82c227b
buyaramadhan <ytchochote@gmail.com>
2025-04-29 03:33:27 +0700

    tambah halaman DetailPiketPage untuk menampilkan detail tugas

---
+++ lib/detail_data_piket.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailPiketPage extends StatelessWidget {
+  final String name; 
+  final String date; 
+  final String task; 
+
+  const DetailPiketPage({
+    Key? key,
+    required this.name,
+    required this.date,
+    required this.task,
+  }) : super(key: key);
+
+ 
