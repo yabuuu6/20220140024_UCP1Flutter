703b997d80230f7b1294b854d1fb8c62d4034c53
buyaramadhan <ytchochote@gmail.com>
2025-04-28 23:34:35 +0700

    atur initialRoute ke LoginPage pada MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -12,3 +12,18 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),   
+      },
+    );
+  }
+}
