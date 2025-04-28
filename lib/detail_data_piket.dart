d4538f41007d6253a57e247f1d3526e8e9ba6c04
buyaramadhan <ytchochote@gmail.com>
2025-04-29 03:38:34 +0700

    Tambahkan layout untuk menampilkan tanggal dan detail tugas pada halaman DetailPiketPage

--- lib/detail_data_piket.dart
+++ lib/detail_data_piket.dart
@@ -25,4 +25,43 @@ class DetailPiketPage extends StatelessWidget {
             Navigator.pop(context);
           },
         ),
-        
+        title: Text(
+          'Detail $task',
+          style: const TextStyle(fontSize: 20, color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    date,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 104, 96, 248),
+                      borderRadius: BorderRadius.circular(8.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    
