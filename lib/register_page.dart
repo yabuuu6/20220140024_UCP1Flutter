07896a159512933a4d2a9cdd9f4667eb8a8bc4cd
buyaramadhan <ytchochote@gmail.com>
2025-04-29 00:07:28 +0700

    buat halaman RegisterPage dengan StatefulWidget untuk form pendaftaran pengguna

---
+++ lib/register_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:ucp1/login_page.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
