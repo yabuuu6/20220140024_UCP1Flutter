e5a5f5b06dee89d7e7d91a3c96d013b7765be3a1
buyaramadhan <ytchochote@gmail.com>
2025-04-28 23:32:54 +0700

    membuat LoginPage dengan parameter data pengguna terdaftar

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,14 @@
-
 import 'package:flutter/material.dart';
-import 'package:ucp1/home_page.dart';
 import 'package:ucp1/login_page.dart';
-
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => const LoginPage(),
-        '/home': (context) => const HomePage(),
-        
-      },
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
+
